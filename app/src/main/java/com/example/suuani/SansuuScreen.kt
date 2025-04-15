67bf6abd9545872993270b0158889440797802bf
仲優樹 <yu-ki216m@outlook.jp>
2025-04-16 08:27:53 +0900

    Added "// TODO : 以下の関数名を「AreaOfRectangle」に修正して画面遷移先で表示する。".

--- app/src/main/java/com/example/suuani/SansuuScreen.kt
+++ app/src/main/java/com/example/suuani/SansuuScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+// TODO : 以下の関数名を「AreaOfRectangle」に修正して画面遷移先で表示する。
 @Composable
 fun SansuuScreen() {
     var width by remember { mutableFloatStateOf(5f) }
