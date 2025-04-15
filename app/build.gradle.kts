a0d16e2bad2517a6d824d01e1c88e47501eb47e8
仲優樹 <yu-ki216m@outlook.jp>
2025-04-16 07:59:13 +0900

    Imported pager libraries.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 }
 
 dependencies {
+    implementation("com.google.accompanist:accompanist-pager-indicators:0.27.1")
+    implementation("com.google.accompanist:accompanist-pager:0.27.1")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
